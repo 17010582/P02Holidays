025a01d9623ce70f9b81fea8af674b1ee7635308
17010582 <17010582@myrp.edu.sg>
2019-04-30 14:51:24 +0800

    Charlotte

--- app/src/main/java/com/charlottechia/p02_holidays/MainActivity.java
+++ app/src/main/java/com/charlottechia/p02_holidays/MainActivity.java
@@ -1,7 +1,10 @@
 package com.charlottechia.p02_holidays;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -17,5 +20,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvHolidays = findViewById(R.id.lvHolidays);
+
+        alHolidays = new ArrayList<String>();
+        alHolidays.add("Secular");
+        alHolidays.add("Ethnic and Religious");
+        aa = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alHolidays);
+        lvHolidays.setAdapter(aa);
+
+        lvHolidays.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+               // Intent intent = new Intent(getBaseContext());
+
+            }
+        });
     }
 }
