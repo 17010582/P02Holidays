cea987cee775df11a6960227feea98ab14fab1db
17010582 <17010582@myrp.edu.sg>
2019-04-30 14:45:48 +0800

    Initial commit

---
+++ app/src/main/java/com/charlottechia/p02_holidays/MainActivity.java
@@ -0,0 +1,21 @@
+package com.charlottechia.p02_holidays;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lvHolidays;
+    ArrayList<String> alHolidays;
+    ArrayAdapter<String> aa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
